88ba6fbc4ea3c87e7599745e02b25727b08fd812
baswa <baswaling1993@gmail.com>
2020-08-28 13:13:18 +0530

    bill.py added to print bill

---
+++ bill.py
@@ -0,0 +1,22 @@
+# This is bill generator for me
+
+
+def create_reciept():
+    drug_list = {}
+    total = 0
+    print("NOTICE : When list is over then type 'end' or 0 to end list")
+    while 1:
+        drug_name = input("Enter the drug name :\n")
+        if drug_name == '0' or drug_name == 'end':
+            break
+        drug_price = input("Drug price :")
+        drug_list[drug_name] = int(drug_price)
+    drug_list_second = drug_list.items()
+
+
+    for drug_name, drug_price in drug_list_second:
+        print(drug_name.capitalize(), '\t', drug_price)
+        total += drug_price
+
+    print("-------------------------------------------------\nTotal amount :", total)
+    return drug_list_second, total
